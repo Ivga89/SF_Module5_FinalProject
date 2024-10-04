3daa8e5eb6052b6feb694443d85011eece96425c
Ivga89 <borozdinaes89@gmail.com>
2024-10-04 23:27:38 +0500

    Collecting. valid string

--- Module5_FinalProject/Program.cs
+++ Module5_FinalProject/Program.cs
@@ -2,6 +2,42 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var userData = CollectUserData();
     }
+
+    static (string name, string lastName, byte age, string[] petNames, string[] favorColors) CollectUserData()
+    {
+        string name;
+        string lastName;
+        byte age;
+        byte countOfPets;
+        byte countOfColors;
+        string[] petNames = null;
+        string[] favorColors = null;
+        bool hasPet;
+
+        do
+        {
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+        } while (!IsValidString(name));
+
+        do
+        {
+            Console.WriteLine("Enter your last name");
+            lastName = Console.ReadLine();
+        } while (IsValidString(lastName));
+    }
+
+    static bool IsValidString(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Incorrect input");
+            return false;
+        }
+        return true;
+    }
+
+    
 }
